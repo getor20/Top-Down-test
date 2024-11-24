195e8811fb8f0a8df697c6afbfb8059f68fbde77
getor20 <ggetor20@gmail.com>
2024-11-24 21:21:40 +0100

    1

--- Assets/Prefab/Player/Player_Script.cs
+++ Assets/Prefab/Player/Player_Script.cs
@@ -9,6 +9,25 @@ public class Player_Script : MonoBehaviour
     [SerializeField]
     private float speed = 10f;
 
+    private bool _isFacingRight = true;
+
+    public bool IsFacingRight
+    {
+        get
+        {
+            return _isFacingRight;
+        }
+        private set
+        {
+            if (_isFacingRight != value)
+            {
+                transform.localScale *= new Vector2(-1  , 1);
+            }
+
+            _isFacingRight = value;
+        }
+    }
+
     private void FixedUpdate()
     {
         rd2D.velocity = new Vector2(vector2.x * speed, vector2.y * speed);
@@ -17,5 +36,19 @@ public class Player_Script : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         vector2 = context.ReadValue<Vector2>();
+
+        SetFacingDirection(vector2);
+    }
+
+    private void SetFacingDirection(Vector2 direction)
+    {
+        if (direction.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true;   
+        }
+        else if (direction.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false;
+        }
     }
 }
