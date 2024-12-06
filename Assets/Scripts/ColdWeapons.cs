fc0b754f32cb16998f36376b1cadeef6c99a446a
getor20 <ggetor20@gmail.com>
2024-12-06 22:35:55 +0100

    1

--- Assets/Scripts/ColdWeapons.cs
+++ Assets/Scripts/ColdWeapons.cs
@@ -1,26 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ColdWeapons : MonoBehaviour
 {
     [SerializeField] 
-    private float _rotationOffset = 90;
-    void Update()
+    private float _rotationOffset = 90f;
+    [SerializeField]
+    private Camera mainCamera;
+    void Update()   
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, angle + _rotationOffset);
+        Vector3 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        Vector3 direction = mousePos - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle - _rotationOffset, Vector3.forward);
+    }
 
-        Vector3 LocalSalle = Vector3.one;
+    public void Attacks(InputAction.CallbackContext context)
+    {
 
-        if(angle > 90 || angle < 90)
-        {
-            LocalSalle.y = -1f;
-        }
-        else
-        {
-            LocalSalle.y = 1f;
-        }
-
-        transform.localScale = LocalSalle;
     }
 }
